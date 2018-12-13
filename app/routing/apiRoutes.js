c88ec04d66174f6f6723ecf0696550162acc96b7
Dane Finch <Dane@Danes-MacBook.local>
2018-12-12 20:19:21 -0800

    Changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,14 +4,14 @@ var friend = require("../data/friend");
 module.exports = function(app) {
 
   app.get("/api/friend", function(req, res) {
-      //console.log("test")
+    
     res.json(friend);
   });
 
 
   app.post("/api/friend", function(req, res) {
       console.log(req.body)
-      //res.send("Hello World")
+    
     var bestMatch = {
         name: "",
         photo: "",
@@ -35,29 +35,17 @@ module.exports = function(app) {
           }
       }
       res.json(bestMatch);
-  });
-
-
-
-//   app.get("/api/friend", function(req, res) {
-//     res.json(waitListData);
-//   });
-
-//   app.post("/api/friend", function(req, res) {
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
-
-//   app.post("/api/clear", function(req, res) {
-//     tableData.length = [];
-//     waitListData.length = [];
-
-//     res.json({ ok: true });
-//   });
-};
+ 
+          var nameFlag = true;
+          for (var i = 0; i < friends.length; i++) {
+              if (user.name == friends[i].name) {
+                  nameFlag = false;
+              }
+          }
+          if (nameFlag) {
+              friends.push(req.body);
+          }
+          res.json(bestMatch);
+      });
+  
+  }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
 
 
   app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
